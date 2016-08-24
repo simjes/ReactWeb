64c4c39c4c6e9e736f9cac6cf5cf5064b9405dfa
Simon Jespersen <s.jespersen@stud.uis.no>
2016-08-24 21:34:45 +0200

    muicss

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,7 @@
 require('normalize.css/normalize.css');
 
 import React from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import HeaderBar from 'components/Header';
-injectTapEventPlugin();
 
 require('styles/App.scss');
 
@@ -12,9 +9,7 @@ require('styles/App.scss');
 class AppComponent extends React.Component {
   render() {
     return (
-        <MuiThemeProvider>
             <HeaderBar />
-        </MuiThemeProvider>
     );
   }
 }
