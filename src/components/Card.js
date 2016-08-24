74103a93b5376ae653977cee8e0f248855b0b39d
Simon Jespersen <s.jespersen@stud.uis.no>
2016-08-25 01:01:59 +0200

    working on cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Button } from 'muicss/react';
 
 class Card extends React.Component {
-
+    render() {
+        return (
+            <div className="card">
+                <div className="card-image-container">
+                    <img src={this.props.image} alt="Avatar" />
+                    <h4>project namesdsds <br /> weeedasdasdsd </h4>
+                </div>
+                <div className="card-container">
+                    <p>
+                        <Button variant="flat"> Android </Button>
+                        <Button variant="flat"> Github </Button>
+                        <Button variant="flat"> Youtube </Button>
+                    </p>
+                </div>
+            </div>
+        )
+    }
 }
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,14 +2,19 @@ require('normalize.css/normalize.css');
 
 import React from 'react';
 import HeaderBar from 'components/Header';
+import Card from 'components/Card';
 
 require('styles/App.scss');
 
+var githubIcon = require('../images/simjes.svg');
 
 class AppComponent extends React.Component {
   render() {
     return (
+        <div>
             <HeaderBar />
+            <Card image={githubIcon} />
+        </div>
     );
   }
 }
