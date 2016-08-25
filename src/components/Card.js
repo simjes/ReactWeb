ccd8ec56efc09b63bc956330ac8dbd881bc5800d
Simon Jespersen <s.jespersen@stud.uis.no>
2016-08-25 19:13:06 +0200

    materializecss cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Button } from 'muicss/react';
+import 'styles/MaterializecssCard.scss';
 
 class Card extends React.Component {
+    renderLinks(links) {
+        var aTags = [];
+        links.forEach( (linkInfo, index) => {
+            aTags.push(<a href={linkInfo.link} target="_blank" key={index}>{linkInfo.title}</a>);
+        });
+        return aTags;
+    }
+
     render() {
+        const links = this.renderLinks(this.props.content.links);
+        console.log(links);
         return (
-            <div className="card">
-                <div className="card-image-container">
-                    <img src={this.props.image} alt="Avatar" />
-                    <h4>project namesdsds <br /> weeedasdasdsd </h4>
+            <div className="card small">
+                <div className="card-image">
+                    <img src={this.props.content.image} />
+                    <span className="card-title">{this.props.content.title}</span>
                 </div>
-                <div className="card-container">
-                    <p>
-                        <Button variant="flat"> Android </Button>
-                        <Button variant="flat"> Github </Button>
-                        <Button variant="flat"> Youtube </Button>
-                    </p>
+                <div className="card-content">
+                    <p>{this.props.content.description}</p>
+                </div>
+                <div className="card-action mui--text-center">
+                    {links}
                 </div>
             </div>
         )
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,38 @@ require('normalize.css/normalize.css');
 import React from 'react';
 import HeaderBar from 'components/Header';
 import Card from 'components/Card';
-
-require('styles/App.scss');
+import {Container} from 'muicss/react';
+import 'styles/App.scss';
 
 var githubIcon = require('../images/simjes.svg');
+var test = {
+    image: githubIcon,
+    title: "super cool title for a sick project",
+    description: "sickest stuff ever",
+    links: [
+        {
+            link: "http://www.tek.no",
+            title: "android"
+        },
+        {
+            link: "http://www.tek.no",
+            title: "youtube"
+        },
+        {
+            link: "http://www.tek.no",
+            title: "Github"
+        }
+    ]
+}
 
 class AppComponent extends React.Component {
   render() {
     return (
         <div>
             <HeaderBar />
-            <Card image={githubIcon} />
+            <Container>
+                <Card content={test} title="weee" />
+            </Container>
         </div>
     );
   }
