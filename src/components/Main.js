0d31c20ea249067704c9e87c656f5bd652586416
Simon Jespersen <s.jespersen@stud.uis.no>
2016-08-29 17:51:31 +0200

    favicon and muicss

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import React from 'react';
 import HeaderBar from 'components/Header';
 import CardContainer from 'components/CardContainer';
 import 'styles/App.scss';
+import 'styles/mui.min.css';
 import FooterBar from 'components/Footer';
 
 class AppComponent extends React.Component {
